e471605c04f6c33c0c96048997721f73c402a394
James Riley Wynne III <wynnejr@ornl.gov>
2014-03-26 15:58:40 -0400

    Imported module file from AccelLinAlgebraLibraries project

---
+++ magma.f90
@@ -0,0 +1,20 @@
+! This module is used to provide FORTRAN interfaces to the C functions and
+! routines provided by MAGMA. To accomplish this, the ISO_C_BINDING module needs
+! to be included so that all the variables are the right types for the C
+! functions.
+module magma
+  Interface
+      Integer function magma_sgesv(n, nrhs, A, ida, ipiv,B, idb, info) BIND (C, NAME="magma_sgesv")
+        use iso_c_binding
+        Implicit none
+        integer (c_int),value::n
+        integer (c_int),value::nrhs
+        real (c_float)::A(*)
+        integer (c_int),value::ida
+        integer (c_int)::ipiv(*)
+        real (c_float)::B(*)
+        integer (c_int),value::idb
+        integer (c_int)::info
+      end function
+   end Interface
+end module
