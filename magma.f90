77f814ca549b055a55b6aaa27966316592a71300
James Riley Wynne III <wynnejr@ornl.gov>
2014-03-31 14:22:29 -0400

    Created test and interface for various other double precision magma functions

--- magma.f90
+++ magma.f90
@@ -31,4 +31,41 @@ module magma
         integer (c_int)::info
       end function
    end Interface
+  Interface
+      Integer function magma_dgetrf(m, n, a, ida, ipiv, info) BIND (C, NAME="magma_dgetrf")
+        use iso_c_binding
+        Implicit none
+        integer (c_int),value::m
+        integer (c_int),value::n
+        real (c_double)::a(*)
+        integer (c_int),value::ida
+        integer (c_int)::ipiv(*)
+        integer (c_int)::info
+      end function
+   end Interface
+  Interface
+      Integer function magma_dgeqrf(m, n, a, ida, tau, work, lwork, info) BIND (C, NAME="magma_dgeqrf")
+        use iso_c_binding
+        Implicit none
+        integer (c_int),value::m
+        integer (c_int),value::n
+        real (c_double)::a(*)
+        integer (c_int),value::ida
+        real (c_double)::tau(*)
+        real (c_double)::work(*)
+        integer (c_int)::lwork
+        integer (c_int)::info
+      end function
+   end Interface
+  Interface
+      Integer function magma_dpotrf(uplo, n, a, ida, info) BIND (C, NAME="magma_dpotrf")
+        use iso_c_binding
+        Implicit none
+        character (c_char) :: uplo
+        integer (c_int),value::n
+        real (c_double)::a(*)
+        integer (c_int),value::ida
+        integer (c_int)::info
+      end function
+   end Interface
 end module
--- test/dgeqrf_test.f90
+++ test/dgeqrf_test.f90
@@ -8,58 +8,51 @@ Program LinearEquations
   Implicit none
 
 ! Declarations, notice single precision
-  Real (KIND=C_DOUBLE) :: A(3,3), b(3)
-  Integer (KIND=C_INT) :: i, j, pivot(3), ok, status
-
+  Real (KIND=C_DOUBLE) :: A(3,3), tau(3), work(3)
+  Integer (KIND=C_INT) :: i, j, lwork = 9, ok, status
 ! Define matrix A
-  A(1,1)=3.1
-  A(1,2)=1.3
-  A(1,3)=-5.7
-  A(2,1)=1.0
-  A(2,2)=-6.9
-  A(2,3)=5.8
-  A(3,1)=3.4
-  A(3,2)=7.2
-  A(3,3)=-8.8
+  A(1,1)=3
+  A(1,2)=1
+  A(1,3)=5
+  A(2,1)=1
+  A(2,2)=6
+  A(2,3)=5
+  A(3,1)=3
+  A(3,2)=7
+  A(3,3)=8
 
 ! Define vector b, make b a matrix and you can solve multiple
 ! equations with the same A but different b
-  b(1)=-1.3
-  b(2)=-0.1
-  b(3)=1.8
-
 ! Find the solution using the MAGMA routine MAGMA_SGESV
-  status = MAGMA_DGESV(3, 1, A, 3, pivot, b, 3, ok)
+  print*, "MAGMA"
+  status = MAGMA_DGEQRF(3, 3, A, 3, tau, work, lwork, ok)
 
 ! Parameters in the order as they appear in the function call
 !    order of matrix A, number of right hand sides (b), matrix A,
 !    leading dimension of A, array that records pivoting, 
 !    result vector b on entry, x on exit, leading dimension of b
 !    return value   
-
-! print the vector x
   do i=1, 3
-     write(*,*) b(i)
+    do j = 1, 3
+      write(*,*) A(i,j)
+    end do
   end do
 
-  A(1,1)=3.1
-  A(1,2)=1.3
-  A(1,3)=-5.7
-  A(2,1)=1.0
-  A(2,2)=-6.9
-  A(2,3)=5.8
-  A(3,1)=3.4
-  A(3,2)=7.2
-  A(3,3)=-8.8
-
-  b(1)=-1.3
-  b(2)=-0.1
-  b(3)=1.8
-
+  A(1,1)=3
+  A(1,2)=1
+  A(1,3)=5
+  A(2,1)=1
+  A(2,2)=6
+  A(2,3)=5
+  A(3,1)=3
+  A(3,2)=7
+  A(3,3)=8
   print*, "LAPACK"
-  call DGESV(3, 1, A, 3, pivot, b, 3, ok)
-
+! print the vector x
+  call DGEQRF(3,3,A,3,tau,work,lwork,ok)
   do i=1, 3
-     write(*,*) b(i)
+    do j = 1, 3
+      write(*,*) A(i,j)
+    end do
   end do
 end
